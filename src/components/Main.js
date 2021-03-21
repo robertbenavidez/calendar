7f42b2db3ba6542ff67a150f9f9f9e6c048dda50
Robert Benavidez <murabitbenavidez@gmail.com>
2021-03-21 11:45:42 -0400

    styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,14 +4,16 @@ import Calendar from './calendar/Calendar'
 
 const Main = () => {
     return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-12">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="row">
-                                <Sidebar />
-                                <Calendar />
+        <div className="wrapper">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="row">
+                                    <Sidebar />
+                                    <Calendar />
+                                </div>
                             </div>
                         </div>
                     </div>
