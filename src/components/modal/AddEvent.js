b9c98878ed56104a571d228c5111bcc8332b8388
Robert Benavidez <murabitbenavidez@gmail.com>
2021-03-31 00:04:01 -0400

    working on the event form. Need to write all the functions on AddEvent.js

--- src/components/modal/AddEvent.js
+++ src/components/modal/AddEvent.js
@@ -2,9 +2,32 @@ import React from 'react'
 import EventForm from './EventForm'
 
 const AddEvent = () => {
+    const inputChange = event => { };
+    const onInputChange = input => { };
+    const onCheckboxChange = () => { };
+    const handleChange = () => { };
+    const closeModal = () => { };
+    const createEvent = () => { };
     return (
         <div >
-            <EventForm />
+            <EventForm
+                modalId='add-event'
+                title='Add Event'
+                closeModal={closeModal}
+                eventName='Watch Movies'
+                checkbox={false}
+                onCheckboxChange={onCheckboxChange}
+                showTime={false}
+                startDate={new Date()}
+                endDate={new Date()}
+                color='Primary'
+                colors={['Primary', 'Danger']}
+                inputChange={inputChange}
+                handleChange={handleChange}
+                onInputChange={onInputChange}
+                eventType={createEvent}
+                buttonText='Save'
+            />
         </div>
     )
 }
--- src/components/modal/EventForm.js
+++ src/components/modal/EventForm.js
@@ -1,15 +1,37 @@
 import React from 'react'
+import DatePicker from 'react-datepicker';
 
-const EventForm = () => {
+import "react-datepicker/dist/react-datepicker.css";
+
+const EventForm = props => {
+    const {
+        modalId,
+        title,
+        closeModal,
+        eventName,
+        inputChange,
+        checkbox,
+        onCheckboxChange,
+        showTime,
+        startDate,
+        endDate,
+        onInputChange,
+        color,
+        colors,
+        handleChange,
+        eventType,
+        buttonText,
+        colorObj,
+    } = props;
     return (
         <div>
-            <div className="modal" id='add-event' tabindex="-1" role="dialog">
+            <div className="modal" id={modalId} tabindex="-1" role="dialog">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title">Modal title</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
+                            <h5 className="modal-title">{title}</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={closeModal}>
+                                X
                             </button>
                         </div>
                         <div className="modal-body p-3">
@@ -21,48 +43,103 @@ const EventForm = () => {
                                         placeholder='Enter Title'
                                         type='text'
                                         name='event-name'
+                                        value={eventName}
+                                        onChange={inputChange}
                                     />
                                 </div>
-                                <div className='form-group'>
+                                <div className='form-check'>
                                     <input
                                         className='form-check-input'
                                         placeholder='Enter Title'
                                         type='checkbox'
                                         name='checkbox'
+                                        value={checkbox}
+                                        checked={checkbox}
+                                        onChange={onCheckboxChange}
                                     />
                                     <label className='control-label'>All day event?</label>
                                 </div>
                                 <div className='form-group'>
                                     <label className='control-label'>Start</label>
-                                    <input
-                                        className='form-control form-white'
-                                        type='date'
-                                        name='event-start'
-                                    />
+                                    <div className='row'>
+                                        {
+                                            !showTime ?
+                                                <div className='col-md-12'>
+                                                    <DatePicker
+                                                        showTimeSelect
+                                                        timeFormat='p'
+                                                        timeIntervals={10}
+                                                        dateFormat='Pp'
+                                                        selected={startDate}
+                                                        onChange={onInputChange('startDate')}
+                                                        className='form-control'
+                                                    />
+                                                </div> :
+                                                <div className='col-md-12'>
+                                                    <DatePicker
+                                                        selected={startDate}
+                                                        onChange={onInputChange('startDate')}
+                                                        className='form-control'
+                                                    />
+                                                </div>
+                                        }
+                                    </div>
                                 </div>
                                 <div className='form-group'>
-                                    <label className='control-label'>End</label>
-                                    <input
-                                        className='form-control form-white'
-                                        type='date'
-                                        name='event-start'
-                                    />
+                                    <label classNam='control-label'>End</label>
+                                    <div className='row'>
+                                        {
+                                            !showTime ?
+                                                <div className='col-md-12'>
+                                                    <DatePicker
+                                                        showTimeSelect
+                                                        timeFormat='p'
+                                                        timeIntervals={10}
+                                                        dateFormat='Pp'
+                                                        selected={endDate}
+                                                        onChange={onInputChange('endDate')}
+                                                        className='form-control'
+                                                    />
+                                                </div> :
+                                                <div className='col-md-12'>
+                                                    <DatePicker
+                                                        selected={endDate}
+                                                        onChange={onInputChange('endDate')}
+                                                        className='form-control'
+                                                    />
+                                                </div>
+                                        }
+                                    </div>
                                 </div>
                                 <div className='form-group'>
                                     <label className='control-label'>Choose Event Color</label>
-                                    <select>
+                                    <select className='form-control form-white' name='event-color' onChange={handleChange}>
                                         <option>Select Color</option>
-                                        <option>Primary</option>
-                                        <option>Info</option>
-                                        <option>Danger</option>
-                                        <option>Success</option>
+                                        {
+                                            colors.map(color =>
+                                                <option value={color.toLowerCase()} key={color}>{color}</option>
+                                            )
+                                        }
                                     </select>
                                 </div>
                             </form>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-primary">Save changes</button>
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button
+                                type="button"
+                                className="btn btn-primary save"
+                                data-dismiss='modal'>
+                                onClick={eventType}
+                                disabled={!eventName || !startDate || !endDate || !color}
+                                {buttonText}
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-light cancel"
+                                data-dismiss="modal"
+                                onClick={closeModal}>
+                                Close
+                            </button>
                         </div>
                     </div>
                 </div>
