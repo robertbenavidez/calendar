94c99bb9644846aaccbb1f84e96b6a29fc1c6198
Robert Benavidez <murabitbenavidez@gmail.com>
2021-04-19 23:18:20 -0400

    new event renders to the calendar date

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 
+import AppContext from '../../context/appContext'
+
 const Calendar = () => {
+    const appContext = useContext(AppContext);
+    const { events } = appContext;
+
     return (
         <div className='col-lg-9'>
             <div>
@@ -16,6 +21,7 @@ const Calendar = () => {
                         center: 'title',
                         right: 'dayGridMonth, timeGridWeek, timeGridDay'
                     }}
+                    events={events}
                 />
             </div>
         </div>
